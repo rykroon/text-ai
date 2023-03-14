fce4d24b731abd980df11f9ebd43e8e0455d2b26
Ryan Kroon <rykroon.tech@gmail.com>
2023-03-13 20:12:23 -0400

    Did a lot of work on the rate limiting.

--- src/utils/ratelimiting.py
+++ src/utils/ratelimiting.py
@@ -1,20 +1,60 @@
+import asyncio
 import time
+
 from redis.asyncio import Redis
 
 
-client = Redis(host='textai-redis')
-pipeline = client.pipeline()
+client = Redis(host="textai-redis")
 
 
-async def ratelimit(identifier: str, duration: int, num_of_requests: int) -> bool:
-    key = f"ratelimit:{identifier}:{duration}"
-    t = time.time()
+async def _ratelimit_helper(key, duration, n, t) -> tuple[bool, float | None]:
+    pipeline = client.pipeline()
     pipeline.zremrangebyscore(key, min=0, max=t - duration)
     pipeline.zcount(key, min=0, max=t)
-    _, count = await pipeline.execute()
-    if count >= num_of_requests:
-        return False
+    pipeline.zrange(key, 0, 0)
+    _, count, scores = await pipeline.execute()
+    if count >= n:
+        lowest_score = float(scores[0])
+        retry_after = duration - (t - lowest_score)
+        return False, retry_after
+    return True, None
+
+
+async def _add_timestamp(key, duration, t):
+    pipeline = client.pipeline()
     pipeline.zadd(key, {t: t})
     pipeline.expire(key, duration)
     await pipeline.execute()
-    return True
+
+
+async def ratelimit(identifier: str, duration: int, n: int) -> tuple[bool, float]:
+    key = f"ratelimit:{identifier}:{duration}"
+    t = time.time()
+
+    success, retry_after = await _ratelimit_helper(key, duration, n, t)
+    if success:
+        await _add_timestamp(key, duration, t)
+
+    return success, retry_after
+
+
+async def multi_ratelimit(identifier: str, mapping: dict[int, int]):
+    t = time.time()
+    coroutines = []
+    for duration, n in mapping.items():
+        key = f"ratelimit:{identifier}:{duration}"
+        coroutines.append(
+            _ratelimit_helper(key, duration, n, t)
+        )
+
+    results = await asyncio.gather(*coroutines)
+    if not all([success for success, _, in results]):
+        retry_after = max(
+            [retry_after for _, retry_after in results if retry_after is not None]
+        )
+        return False, retry_after
+
+    for duration, _ in mapping.items():
+        key = f"ratelimit:{identifier}:{duration}"
+        await _add_timestamp(key, duration, t)
+    return True, None
